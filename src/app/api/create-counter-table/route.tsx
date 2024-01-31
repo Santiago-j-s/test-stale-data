5a3c0f2d0b2280b2b0f52ac395b17f26ee12823b
Santiago Santana <santana.santiago@gmail.com>
2024-01-30 22:19:46 -0300

    fix

--- src/app/api/create-counter-table/route.tsx
+++ src/app/api/create-counter-table/route.tsx
@@ -3,7 +3,8 @@ import { NextResponse } from "next/server";
 
 export async function GET() {
   try {
-    const result = await sql`CREATE TABLE counter (cnt  INTEGER);`;
+    const result =
+      await sql`CREATE TABLE counter (cnt  INTEGER); INSERT INTO counter (cnt) VALUES (0);`;
     return NextResponse.json({ result }, { status: 200 });
   } catch (error) {
     return NextResponse.json({ error }, { status: 500 });
